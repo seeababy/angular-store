ec519d0e3f38910066fd66f678d88d837d774d4c
Yegor <test@gmail.com>
2025-11-23 16:52:58 +0200

    add product card

--- src/app/feature/home/pages/home-page/home-page.ts
+++ src/app/feature/home/pages/home-page/home-page.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Product } from '../../../../shared/models/product.model';
+import { Card } from '../../../../shared/components/card/card';
 
 @Component({
   selector: 'app-home-page',
@@ -6,6 +8,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [],
+  imports: [Card],
 })
-export class HomePage { }
+export class HomePage { 
+  products: Product[] = [
+    { id: 1, title: 'iPhone 17 pro max 256GB', price: 66999, image: '/assets/images/product-card/iphone17promax256gb.webp' },
+  ];
+
+  addToCart(productId: number) {
+    console.log('Added product:', productId);
+  }
+}
